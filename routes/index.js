5fb79d209b46d2c94a29d8ef15f5aa9bea5abaea
maxht <maxhthomas@outlook.com>
2021-12-11 02:38:43 +0000

    Reviews can be posted and searched.

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+let reviews = [{title: "Title example", body: "Body example"},
+               {title: "Title example 2", body: "Body example 2"},
+               {title: "", body: ""}]
+
 router.get("/", (req, res) => {
-    const reviews = [
-        {title: "Title example", body: "Body example"},
-        {title: "Title example 2", body: "Body example 2"},
-        {title: "", body: ""}
-    ]
     res.render("index.ejs", {reviews})
 });
 
 router.get("/:id", (req, res) => {
     const id = req.params.id;
-    res.json(id)
+    try {
+        res.json(reviews[id].title)
+    } catch {
+        res.json(id)
+    }
+        
 });
 
 router.post("/post", (req, res) => {
     title = req.body.reviewTitle
     body = req.body.reviewBody
-    console.log(title, body)
+    reviews.push({title:title, body:body})
     
     res.redirect("/")
 });
